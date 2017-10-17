11f864eca3b42f8a6a2b5d2399562b37caee43d8
Kimberly Bordon <kbordon@gmail.com>
2017-10-16 20:31:12 -0700

    create solution folder with project and start tests code.

---
+++ QueenAttack/Models/Queen.cs
@@ -0,0 +1,11 @@
+namespace QueenAttack
+{
+    public class Queen
+    {
+        public bool CanAttack(int queenX, int queenY, int otherX, int otherY)
+        {
+            // code will go here
+            return false;
+        }
+    }
+}
